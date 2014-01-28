41776372bc215636d5d1c3f176896cf8a5c3f05f
dilip kushwaha <kush.dilip@gmail.com>
2014-01-28 12:30:07 +0530

    first commit

---
+++ DataConversion/StudentModule/MainProgram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+
+namespace DataConversion
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            string csvFilePath = @".\Resources\StudentMarks.csv";
+            string xmlFilePath = @".\Resources\StudentMarks.xml";
+
+            //Converting CSV to XML, comment out after running once.
+            convertCsvToXml(csvFilePath, xmlFilePath);
+
+            //Converting XML to C# List
+            var studentsMarks = convertXMLtoList(xmlFilePath);
+
+            foreach (var student in studentsMarks)
+            {
+                Console.Out.WriteLine(student.Physics);
+            }
+        }
+
+
+
+        public static void convertCsvToXml(string csvFilePath, string xmlFilePath)
+        {
+            var lines = File.ReadAllLines(csvFilePath);
+            string[] headers = lines[0].Split(',').Select(x => x).ToArray(); //Getting header values from first line  
+            var xml = new XElement("StudentMarks",
+              lines.Where((line, index) => index > 0)
+              .Select((line, i) => new XElement("Student",
+                 line.Split(',').Select((column, index) => new XElement(headers[index], column)))));
+            xml.Save(xmlFilePath);
+        }
+
+        public static List<Student> convertXMLtoList(string filePath)
+        {
+            XDocument doc = XDocument.Load(filePath);
+            List<Student> studentsMarks = doc.Descendants("Student").Select(x => new Student()
+            {
+                RollNo = int.Parse(x.Element("roll_no").Value),
+                Name = x.Element("name").Value,
+                Physics = short.Parse(x.Element("physics").Value),
+                Chemistry = short.Parse(x.Element("chemistry").Value),
+                Maths = short.Parse(x.Element("maths").Value),
+                Biology = short.Parse(x.Element("biology").Value),
+                English = short.Parse(x.Element("english").Value)
+            }).ToList();
+
+            return studentsMarks;
+        }
+
+    }
+}
---
+++ DataConversion/StudentModule/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataConversion
+{
+    public class Student
+    {
+        public int RollNo {get; set;}
+        public string Name { get; set; }
+        public short Physics { get; set; }
+        public short Chemistry { get; set; }
+        public short Maths { get; set; }
+        public short Biology { get; set; }
+        public short English { get; set; }
+    }
+}
